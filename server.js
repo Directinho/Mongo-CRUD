3e2bd9aef7f15052b96d1dcc7758bfa86c82c0e9
TASS154 <121037641+TASS154@users.noreply.github.com>
2024-08-28 12:42:00 -0300

    ADD e EXCLUDE

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 const methodOverride = require('method-override');
 
-app.use(espress.son());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
 
@@ -31,7 +31,7 @@ app.post('/cadastro', async (req, res) => {
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
 
-        const result = await collection.insertOne(novolibro);
+        const result = await collection.insertOne(novoLivro);
         console.log(`Livro cadastrado com sucesso. ID: ${result.insertedId}`);
 
         res.redirect('/')
@@ -42,6 +42,58 @@ app.post('/cadastro', async (req, res) => {
         client.close();
     }
 });
+
+app.post('/deletar', async (req, res) => {
+    const { id } = req.body;
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName)
+
+        const result = await collection.deleteOne({ _id: new ObjectId(id)});
+
+        if (result.deletedCount > 0){
+            res.redirect('/');
+        } else {
+            res.status(404).send('Livro não encontrado.');
+        }
+    } catch (err) {
+        console.error('Erro ao deletar o livro:', err);
+        res.status(500).send(
+            'Erro ao deletar o livro. por favor tente novamente mais tarde'
+        )
+    } finally {
+        client.close();
+    }
+});
+
+app.get('/livros', async (req, res) => {
+    const client = new MongoClient(url)
+
+    try {
+        await client.connect()
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+    
+        const livros = await collection.find({}, {
+            projection: { _id: 1, titulo: 1, autor: 1, editora:1 }
+        }).toArray();
+
+        res.json(livros);
+    } catch (err) {
+        console.error('Erro ao buscar livros:', err);
+        res.status(500).send(
+            'Erro ao buscar livros, porfavor, tente novamente mais tarde'
+        );
+    } finally {
+        client.close();
+    }
+
+
+})
 
 app.listen(port, () => {
     console.log(`Servidor Node.js em execução em http://localhost:${port}`);
