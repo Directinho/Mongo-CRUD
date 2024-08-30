9e81317f9f2289efec33e014fb79a0603c9f78e9
DirectDev <69155843+Directinho@users.noreply.github.com>
2024-08-30 07:59:05 -0300

    Atualização da ultima aula no server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const methodOverride = require('method-override');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const url = 'mongodb://127.0.0.1:27017/';
@@ -53,9 +53,9 @@ app.post('/deletar', async (req, res) => {
         const db = client.db(dbName);
         const collection = db.collection(collectionName)
 
-        const result = await collection.deleteOne({ _id: new ObjectId(id)});
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
 
-        if (result.deletedCount > 0){
+        if (result.deletedCount > 0) {
             res.redirect('/');
         } else {
             res.status(404).send('Livro não encontrado.');
@@ -77,9 +77,9 @@ app.get('/livros', async (req, res) => {
         await client.connect()
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
-    
+
         const livros = await collection.find({}, {
-            projection: { _id: 1, titulo: 1, autor: 1, editora:1 }
+            projection: { _id: 1, titulo: 1, autor: 1, editora: 1 }
         }).toArray();
 
         res.json(livros);
@@ -91,7 +91,30 @@ app.get('/livros', async (req, res) => {
     } finally {
         client.close();
     }
+    app.get('/livro/:id', async (req, res) => {
+        const { id } = req.params;
 
+        const client = new MongoClient(url);
+
+        try {
+            await client.connect();
+            const db = client.db(dbName);
+            const collection = db.collection(collectionName);
+
+            const livro = await collection.findOne({ _id: new ObjectId(id) });
+
+            if (!livro) {
+                return res.status(404).send('Livro não encontrado');
+            }
+
+            res.json(livro);
+        } catch (err) {
+            console.error('Erro ao buscar o livro: ', err);
+            res.status(500).send('Erro ao buscar o livro. Por favor, tente novamente mais tarde!')
+        } finally {
+            client.close();
+        }
+    })
 
 })
 
